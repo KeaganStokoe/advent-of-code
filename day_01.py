d2c0890872024812faabaeb006fdf73845c34f6b
Keagan-EXPLORE <keagan@explore-ai.net>
2021-12-01 12:47:53 +0200

    AoC Day 1

---
+++ day_01.py
@@ -0,0 +1,14 @@
+formatted_list = []
+counter = 0
+with open('01_input.txt', 'rb') as f:
+    mylist = f.read().splitlines()
+    for i in mylist:
+        formatted_list.append(int(i))
+
+for index, value in enumerate(formatted_list):
+    if index == 0:
+        continue
+    if value > formatted_list[index - 1]:
+        counter += 1
+
+print(counter)
