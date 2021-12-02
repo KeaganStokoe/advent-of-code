12d1058fd5d971f7a2d29be529d114e40db49ff7
Keagan-EXPLORE <keagan@explore-ai.net>
2021-12-02 10:12:55 +0200

    solution - day 2, challenge 1

---
+++ day_02/day_02_challenge_01.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+let horizontal = 0;
+let depth = 0;
+
+fs.readFile("./day_02_input.txt", (err, data) => {
+  if (err) throw err;
+
+  const formatted_data = data.toString();
+  const input_array = formatted_data.split("\n");
+
+  for (let item of input_array) {
+    const item_data = item.split(" ");
+    if (item_data[0] === "forward") {
+      horizontal += parseInt(item_data[1]);
+    } else if (item_data[0] === "down") {
+      depth -= parseInt(item_data[1]);
+    } else if (item_data[0] === "up") {
+      depth += parseInt(item_data[1]);
+    }
+  }
+
+  let final_result = Math.abs(horizontal) * Math.abs(depth);
+  console.log("🚀 Total:", final_result);
+});
